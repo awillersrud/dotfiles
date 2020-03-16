0c7a8b513a33b9259ac3bfc267d4e0d857a2839d
Asbjørn Willersrud <asbjorn.willersrud@embriq.no>
2020-03-16 12:51:43 +0100

    Disable prompt status code timer fields

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -14,7 +14,7 @@ set_prompt() {
 	PS1+="%{$fg_bold[white]%}${PWD/#$HOME/~}%{$reset_color%}"
 
 	# Status Code
-	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
+	#PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
 
  	# Git
  	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
@@ -28,10 +28,10 @@ set_prompt() {
 
 
 	# Timer: http://stackoverflow.com/questions/2704635/is-there-a-way-to-find-the-running-time-of-the-last-executed-command-in-the-shel
-	if [[ $_elapsed[-1] -ne 0 ]]; then
-		PS1+=', '
-		PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
-	fi
+	#if [[ $_elapsed[-1] -ne 0 ]]; then
+	#	PS1+=', '
+	#	PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
+	#fi
 
 	# PID
 	if [[ $! -ne 0 ]]; then
